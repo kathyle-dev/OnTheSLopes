f74d5bd13dec11ea572816bba28e25dc742c49df
Amin Elhami <57550077+Amin747@users.noreply.github.com>
2021-03-18 08:47:10 -0700

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -1,31 +1,55 @@
 document.getElementById("loc_btn").addEventListener("click", getLocation);
 var loc = document.getElementById("location");
 function getLocation() {
-if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(savePosition);
-sendCoordinates();
-} else {
-loc.innerHTML = "Geolocation is not supported by this browser.";
-}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(savePosition);
+    sendCoordinates();
+  } else {
+    loc.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 let body;
 function savePosition(position) {
-body = {
-longitude : position.coords.longitude,
-latitude : position.coords.latitude
-}
+  body = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude,
+  };
 }
 
 //send the form inputs to a backend route
-function sendCoordinates(){
-const URL = "/resorts"
-fetch(URL, {
-method: "GET",
-headers: {
-"Content-Type": "application/json",
-},
-body: JSON.stringify(body),
-})
-.then((res) => res.json())
-.then(data => console.log(data, "Front end fetch resorts"))
+function sendCoordinates() {
+  const URL = "/resorts";
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    //.then((data) => console.log(data))
+    .then((data) => displayResorts(data))
+    .catch((err) => console.log(err));
+}
+let cards = "";
+
+function displayResorts(data) {
+  for (let i = 0; i < data.length; i++) {
+    //console.log(data[i]);
+    let name = data[i].name;
+    //let picture = data[i].photos[0].photo_reference;
+    let rating = data[i].rating;
+    // let {
+    //   opening_hours: { open_now },
+    // } = data[i];
+    // isOpen = isOpen.toString();
+    // console.log(open_now);
+    cards += `<div class="card" style="width: 18rem;">
+        <div class="card_class">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${rating}</p>
+        </div>
+    </div>`;
+  }
+  document.getElementById("resorts_container").innerHTML = cards;
 }
